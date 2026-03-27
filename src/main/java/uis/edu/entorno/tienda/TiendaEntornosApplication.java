742ff52724da6b93fb4bfaef4ae4b9eb4cd650d1
Cristian Camilo Carreño Rey <cristian2221475@correo.uis.edu.co>
2026-03-27 17:01:16 -0500

    Proyecto inicial TiendaEntornos

---
+++ src/main/java/uis/edu/entorno/tienda/TiendaEntornosApplication.java
@@ -0,0 +1,13 @@
+package uis.edu.entorno.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaEntornosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaEntornosApplication.class, args);
+	}
+
+}
